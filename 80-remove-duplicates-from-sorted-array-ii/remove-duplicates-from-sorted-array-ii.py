29bade69bfc954adf8add85320fc47319fc86c69
maheshchhedal <maheshchhedal0@gmail.com>
2026-08-06 08:39:32 +0545

    Time: 73 ms (41.86%) | Memory: 15.2 MB (67.77%) - LeetSync

---
+++ 80-remove-duplicates-from-sorted-array-ii/remove-duplicates-from-sorted-array-ii.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        n=len(nums)
+        start=1
+        for i in range(2,n):
+            if nums[i]!=nums[start-1]:
+                start +=1
+                nums[start]=nums[i]
+        return start +1
+        
